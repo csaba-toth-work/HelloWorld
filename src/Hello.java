d4bcf88608bc7657839dedebc99ad51c45b51374
Csaba Toth <csaba.toth.work@protonmail.com>
2021-04-16 10:50:51 +0200

    last one changed to 3000

--- src/Hello.java
+++ src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
         int mySecondNumber = 12;
         int myThirdNumber = myFirstNumber * 2;
         int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
-        int myLastOne = 1000 - myTotal;
+        int myLastOne = 3000 - myTotal;
 
         System.out.println(myTotal);
         System.out.println(myLastOne);
